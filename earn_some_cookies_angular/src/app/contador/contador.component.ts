2a4638e5e562f96c076022b5976b8cc909677f94
Daniel Nager Carpio <danielnagercarpios@gmail.com>
2024-02-15 22:02:05 +0100

    first commit

---
+++ earn_some_cookies_angular/src/app/contador/contador.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contador',
+  templateUrl: './contador.component.html',
+  stylesUrl: ['./contador.component.scss']
+})
+
+class ContadorComponent (
+
+)
